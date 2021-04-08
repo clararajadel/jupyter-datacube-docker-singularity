153d001844671a41c4db43861def0364d3ee7464
clararajadel <rajadellambistos@gmail.com>
2021-04-08 15:25:43 +0200

    Dockerfile

---
+++ local/scripts/entrypoint.sh
@@ -0,0 +1,4 @@
+/usr/local/bin/jupyter-notebook --config=/app/conf/jupyter.py
+
+# /app is because the environment in the Dockerfile will be named /app and singularity needs the entire path to build the image.
+
