6c526c6b1474621a7ba4d263b91a8ad2ba225f8f
clararajadel <rajadellambistos@gmail.com>
2021-04-08 13:34:01 +0200

    jupyter config

---
+++ local/conf/jupyter.py
@@ -0,0 +1,16 @@
+import os
+c = get_config()
+# Kernel config
+c.IPKernelApp.pylab = 'inline'  # if you want plotting support always in your notebook
+# Notebook config
+c.NotebookApp.notebook_dir = 'nbs'
+c.NotebookApp.allow_origin = '*' # put your public IP Address here or * to allow all
+c.NotebookApp.ip = '*'
+c.NotebookApp.allow_remote_access = True
+c.NotebookApp.open_browser = False
+c.NotebookApp.password = u'argon2:$argon2id$v=19$m=10240,t=10,p=8$qJRSNqPjEqzc/O97Wzb/Rg$bP+S2ixO8Zh3N/h4HRobxg'
+c.NotebookApp.port = int(os.environ.get("PORT", 5200))
+c.NotebookApp.allow_root = True
+c.NotebookApp.allow_password_change = True
+c.ConfigurableHTTPProxy.command = ['configurable-http-proxy', '--redirect-port', '80']
+
